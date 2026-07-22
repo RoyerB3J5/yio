3dc83e41c37e9e5ddf39f58d450378152358d5f3
Royer Bonifacio <royer.boni13@gmail.com>
2026-07-22 01:12:18 -0500

    fix:button

--- components/shop/CartDrawer.tsx
+++ components/shop/CartDrawer.tsx
@@ -156,7 +156,6 @@ export default function CartDrawer() {
               onClick={handleClick}
               disabled={loading}
               label={content.checkout}
-              styleButton="black"
               paddingX="px-6"
               wFull={true}
             />
